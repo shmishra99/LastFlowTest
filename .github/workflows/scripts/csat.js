8b6682ee07b55e6aaf49a5f776f566676eb0a8cd
shmishra99 <shivammishr@google.com>
2023-06-19 14:17:38 +0530

    file test

--- .github/workflows/scripts/csat.js
+++ .github/workflows/scripts/csat.js
@@ -25,18 +25,35 @@ module.exports = async ({github, context}) => {
         baseUrl = CONSTANT_VALUES.MODULE.CSAT.MEDIA_PIPE_BASE_URL;
       else
         baseUrl = CONSTANT_VALUES.MODULE.CSAT.BASE_URL;
-      const yesCsat =
-          (CONSTANT_VALUES.MODULE.CSAT.YES)
-              .link(
-                  baseUrl + CONSTANT_VALUES.MODULE.CSAT.SATISFACTION_PARAM +
-                  CONSTANT_VALUES.MODULE.CSAT.YES +
-                  CONSTANT_VALUES.MODULE.CSAT.ISSUEID_PARAM + issue);
-      const noCsat =
-          (CONSTANT_VALUES.MODULE.CSAT.NO)
-              .link(
-                  baseUrl + CONSTANT_VALUES.MODULE.CSAT.SATISFACTION_PARAM +
-                  CONSTANT_VALUES.MODULE.CSAT.NO +
-                  CONSTANT_VALUES.MODULE.CSAT.ISSUEID_PARAM + issue);
+    
+      const yesCsat = document.createElement("a");
+      yesCsat.textContent = CONSTANT_VALUES.MODULE.CSAT.YES;
+      yesCsat.href =  baseUrl + CONSTANT_VALUES.MODULE.CSAT.SATISFACTION_PARAM +
+      CONSTANT_VALUES.MODULE.CSAT.YES +
+      CONSTANT_VALUES.MODULE.CSAT.ISSUEID_PARAM + issue
+      
+      const noCsat = document.createElement("a");
+      noCsat.textContent = CONSTANT_VALUES.MODULE.CSAT.NO
+      noCsat.href =  baseUrl + CONSTANT_VALUES.MODULE.CSAT.SATISFACTION_PARAM +
+      CONSTANT_VALUES.MODULE.CSAT.NO +
+      CONSTANT_VALUES.MODULE.CSAT.ISSUEID_PARAM + issue
+
+
+
+
+
+    //   const yesCsat =
+    //       (CONSTANT_VALUES.MODULE.CSAT.YES)
+    //           .link(
+    //               baseUrl + CONSTANT_VALUES.MODULE.CSAT.SATISFACTION_PARAM +
+    //               CONSTANT_VALUES.MODULE.CSAT.YES +
+    //               CONSTANT_VALUES.MODULE.CSAT.ISSUEID_PARAM + issue);
+    //   const noCsat =
+    //       (CONSTANT_VALUES.MODULE.CSAT.NO)
+    //           .link(
+    //               baseUrl + CONSTANT_VALUES.MODULE.CSAT.SATISFACTION_PARAM +
+    //               CONSTANT_VALUES.MODULE.CSAT.NO +
+    //               CONSTANT_VALUES.MODULE.CSAT.ISSUEID_PARAM + issue);
       const comment = CONSTANT_VALUES.MODULE.CSAT.MSG + '\n' + yesCsat + '\n' +
           noCsat + '\n';
       let issueNumber = context.issue.number ?? context.payload.issue.number;
