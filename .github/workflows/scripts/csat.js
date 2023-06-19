de9542fc89c1b90d0aac75d5656f2e7d24442763
shmishra99 <shivammishr@google.com>
2023-06-19 14:50:25 +0530

    file test

--- .github/workflows/scripts/csat.js
+++ .github/workflows/scripts/csat.js
@@ -26,13 +26,13 @@ module.exports = async ({github, context}) => {
       else
         baseUrl = CONSTANT_VALUES.MODULE.CSAT.BASE_URL;
     
-      const yesCsat = `<a href=${ baseUrl + CONSTANT_VALUES.MODULE.CSAT.SATISFACTION_PARAM +
+      const yesCsat = `<a href="${ baseUrl + CONSTANT_VALUES.MODULE.CSAT.SATISFACTION_PARAM +
         CONSTANT_VALUES.MODULE.CSAT.YES +
-        CONSTANT_VALUES.MODULE.CSAT.ISSUEID_PARAM + issue}> ${CONSTANT_VALUES.MODULE.CSAT.YES}</a>`
+        CONSTANT_VALUES.MODULE.CSAT.ISSUEID_PARAM + issue}"> ${CONSTANT_VALUES.MODULE.CSAT.YES}</a>`
       
-        const noCsat = `<a href=${ baseUrl + CONSTANT_VALUES.MODULE.CSAT.SATISFACTION_PARAM +
+        const noCsat = `<a href="${ baseUrl + CONSTANT_VALUES.MODULE.CSAT.SATISFACTION_PARAM +
             CONSTANT_VALUES.MODULE.CSAT.NO +
-            CONSTANT_VALUES.MODULE.CSAT.ISSUEID_PARAM + issue}> ${CONSTANT_VALUES.MODULE.CSAT.NO}</a>`
+            CONSTANT_VALUES.MODULE.CSAT.ISSUEID_PARAM + issue}"> ${CONSTANT_VALUES.MODULE.CSAT.NO}</a>`
 
 
 
